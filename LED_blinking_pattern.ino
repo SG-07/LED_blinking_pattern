9c82a1558782dd3dc0661fc25ea7646ee7428639
SG-07 <satyamphoenix07@gmail.com>
2020-12-21 20:20:45 +0530

    Removed code which is not required.

--- LED_blinking_pattern.ino
+++ LED_blinking_pattern.ino
@@ -7,18 +7,6 @@ void setup() {
   for(int i=2; i<=13;i++){
     pinMode(i,OUTPUT);
     }
-    /*pinMode(2,OUTPUT);
-      pinMode(3,OUTPUT);
-      pinMode(4,OUTPUT);
-      pinMode(5,OUTPUT);
-      pinMode(6,OUTPUT);
-      pinMode(7,OUTPUT);
-      pinMode(8,OUTPUT);
-      pinMode(9,OUTPUT);
-      pinMode(10,OUTPUT);
-      pinMode(11,OUTPUT);
-      pinMode(12,OUTPUT);
-      pinMode(13,OUTPUT);*/
 
 
 }
